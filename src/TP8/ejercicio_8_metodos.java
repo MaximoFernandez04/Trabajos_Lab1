82834b02a0802bf22c70517b857b48a192483746
Maximo <maxife924@gmail.com>
2023-11-12 13:45:56 -0300

    TP8

---
+++ src/TP8/ejercicio_8_metodos.java
@@ -0,0 +1,33 @@
+package TP8;
+
+import java.util.Scanner;
+
+public class ejercicio_8_metodos {
+    public static void main(String[] args) {
+        //Ejercicio 1
+        /*
+        int filas, columnas;
+
+        System.out.println("Matriz 1:");
+        filas = metodos_tp8.leerEntero();
+        columnas = metodos_tp8.leerEntero();
+        int[][] matriz1 = metodos_tp8.crearMatriz(filas, columnas);
+        metodos_tp8.llenarMatriz(matriz1);
+
+        System.out.println("Matriz 2:");
+        filas = metodos_tp8.leerEntero();
+        columnas = metodos_tp8.leerEntero();
+        int[][] matriz2 = metodos_tp8.crearMatriz(filas, columnas);
+        metodos_tp8.llenarMatriz(matriz2);
+
+        metodos_tp8.mostrarMatriz(matriz1);
+        metodos_tp8.mostrarMatriz(matriz2);
+
+        int[][] resultado = metodos_tp8.sumarMatrices(matriz1, matriz2);
+
+        System.out.println("Resultado de la suma de matrices:");
+        metodos_tp8.mostrarMatriz(resultado);
+         */
+        
+    }
+}
